27e6cf8c4391e4e144faa16b19bb779c9c4b9cbb
Matthew Smith <matthewsmith2003@yahoo.com>
2025-03-03 15:52:38 -0500

    Update skill extraction algorithm

--- backend/utils/skillextract.py
+++ backend/utils/skillextract.py
@@ -1,40 +1,44 @@
 import re
 
 skills = {
-        "Languages": ["Python", "Java", "C++", "R", "C#", "C"],
-        "Frameworks": ["Node", "Angular"],
+        "Languages": ["Python", "Java", "C++", "R", "C#", "C", "ANSI C", "Objective-C", "Visual Basic"],
+        "Frameworks": ["Node.js", "Angular", "Express.js"],
         "Database Management": ["SQL", "PostGreSQL", "NoSQL", "MongoDB"],
         "Web Development": ["JavaScript", "HTML", "CSS"],
         "Tools": ["Git", "Docker", "Google Cloud", "Azure", "AWS"]
-        }
+}
 
 #test description
-desc = "Prospective applicants will need at least 3 years experience with Python, C#, PostGreSQL, and Google Cloud. Python is important."
+desc = ("Prospective applicants will need at least 3 years experience with C++, Python, C, C#, PostGreSQL,"
+        " and Google Cloud. Python is important. Applicants should also have experience with Node.js."
+        " Visual\nBasic too.")
+
+# When searching for skills, we check if they are surrounded by
+# any two characters from this expression
+ignore = " !\"\'\\`()\n\t,:;=<>?./"
+
 
 #function needs to eventually take job description, iterate through, extract the required skills, and assign them to the job entry in the database
-def skillExtract(job_desc):
-        
-        lowerDesc = job_desc.lower()
+def skill_extract(job_desc: str) -> list:
+        # Turn job description to lowercase and pad it with spaces for regex search
+        job_desc = " " + job_desc.lower() + " "
+
+        # Replace characters
+        char_map = {"\n": " ", "\t": " ", "\\": ""}
+        job_desc = job_desc.translate(str.maketrans(char_map))
 
         #initialize skillset list
         skillset = []
 
-        #for skills that are not C++ or C#, find those in job description
-        for category, skill_list in skills.items():
+        # Search job description for each skill, one at a time
+        for skill_list in skills.values():
                 for skill in skill_list:
-                        pattern = r'\b' + re.escape(skill.lower()) + r'\b' #this ignores special characters - C++ and C# are recognized incorrectly as C
-                        if re.search(pattern, lowerDesc):
+                        pattern = r"[" + ignore + r"]" + re.escape(skill.lower()) + r"[" + ignore + r"]"
+                        if re.search(pattern, job_desc):
                                 skillset.append(skill)
-
-        #hardcode C++ and C# in until more elegant solution is found
-        for skill in ["C++", "C#"]:
-                if skill.lower() in lowerDesc:
-                        skillset.append(skill)
-
-        splitDesc = job_desc.split() 
-        if "C" not in splitDesc and "C" in skillset: #account for C being incorrectly recognized when not actually in desc
-               skillset.remove("C")
 
         return skillset
 
-print(skillExtract(desc))
+
+if __name__ == "__main__":
+        print(skill_extract(desc))
