194302f53a929032f8f44455e7ceb8b46818ee7f
cfp-rbg <cfp4@hood.edu>
2025-03-10 17:08:10 -0400

    Database implementation complete

--- backend/jobsearch/admin.py
+++ backend/jobsearch/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Skill, User, Job, AltSkill
 
 # Register your models here.
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('id', 'skill_name', 'category')
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user_name', 'password', 'city', 'state', 'education', 'years_exp', 'skill_name')
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('id', 'site', 'job_name', 'job_type', 'job_desc', 'company', 'city', 'state', 'min_sal', 'is_remote', 'post_date', 'years_exp', 'skill_name')
+
+class AltSkillAdmin(admin.ModelAdmin):
+    list_display = ('id', 'skill_name', 'alt_name')
+
+admin.site.register(Skill, SkillAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Job, JobAdmin)
+admin.site.register(AltSkill, AltSkillAdmin)
--- backend/jobsearch/models.py
+++ backend/jobsearch/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Skill(models.Model):
+    skill_name = models.CharField(max_length=20)
+    category = models.CharField(max_length=20)
+
+class User(models.Model):
+    user_name = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    city = models.CharField(max_length=20, blank=True)
+    state = models.CharField(max_length=2, blank=True)
+    education = models.CharField(max_length=20, blank=True)
+    years_exp = models.IntegerField(null=True)
+    skill_name = models.ForeignKey(Skill, on_delete=models.CASCADE)
+
+class Job(models.Model):
+    site = models.CharField(max_length=10)
+    job_name = models.CharField(max_length=50)
+    job_type = models.CharField(max_length=10, blank=True)
+    job_desc = models.CharField(max_length=200)
+    company = models.CharField(max_length=50)
+    city = models.CharField(max_length=20)
+    state = models.CharField(max_length=2)
+    min_sal = models.FloatField(null=True)
+    is_remote = models.BooleanField()
+    post_date = models.DateField()
+    years_exp = models.IntegerField(null=True)
+    skill_name = models.ForeignKey(Skill, on_delete=models.CASCADE)
+
+
+class AltSkill(models.Model):
+    skill_name = models.ForeignKey(Skill, related_name='alt_names', on_delete=models.CASCADE)
+    alt_name = models.CharField(max_length=20)
