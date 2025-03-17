843a0a432814ec174c4a6d90232412b4268a9212
cfp-rbg <cfp4@hood.edu>
2025-03-16 21:51:48 -0400

    Debug backend functions, specifically fix how dictionaries are formatted in get-static-data

--- backend/jobsearch/views.py
+++ backend/jobsearch/views.py
@@ -1,28 +1,29 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 import json
-from .models import Skill, User, Job, AltSkill, City, County, State
-from utils import calc_compatibility
+from .models import Skill, Job, City, County, State
+from utils.calc_compatibility import calculate_compatibility
 import string
 
 # Create your views here.
 
-def skill_search(userState): #assume userState is the state's id
+def skill_search(request, userState): #assume userState is the state's id
     #Output: JSON dictionary containing the following: List of skills, each with an associated integer representing the number of descriptions that mention that skill
     #for each skill in db
         #find all jobs requiring that skill within specified state
     skillSet = Skill.objects.values_list('skill_name', flat=True)
     counts = []
     for skill in skillSet:
-        jobList = [Job.objects.filter(Job.skills==skill, Job.city==City, City.county==County, County.state==State, State.pk==userState).values(Job)]
+        jobList = [Job.objects.filter(Job.skills==skill, Job.city==City, City.county==County, County.state==State, State.pk==userState).values(Job.pk)]
         counts.append(len(jobList))
         
     skill_counts = dict(zip(skillSet, counts))
 
-    return json.dumps(skill_counts)
+    return JsonResponse({'skill counts': skill_counts})
 
     #After midterm: Dictionary of U.S. states, where each state is a dictionary of (county, most_common_skill) pairs
 
-def job_search(skillSet, edu, yearsExp, userState): #assume userState is the state's id
+def job_search(request, skillSet, edu, yearsExp, userState): #assume userState is the state's id
     #Output: JSON dictionary consisting of a list of jobs. Each job should itself be a Python dictionary consisting of the title, location, description, salary, link to apply, and compatibility score.
     jobList = []
     jobs = Job.objects.filter(Job.city==City, City.county==County, County.state==State, State==userState)
@@ -30,22 +31,24 @@ def job_search(skillSet, edu, yearsExp, userState): #assume userState is the sta
         reqSkills = list(job.skills)
         reqEdu = string(job.education)
         reqYears = int(job.years_exp)
-        score = calc_compatibility(skillSet, edu, yearsExp, reqSkills, reqEdu, reqYears)
+        score = calculate_compatibility(skillSet, edu, yearsExp, reqSkills, reqEdu, reqYears)
         jobList.append({job.job_name, job.city, job.job_desc, job.min_sal, job.url, job.company, score})
 
-    return json.dumps(jobList)
+    return JsonResponse({'jobs': jobList})
 
-def get_static_data():
-    #Output: JSON dictionary consisting of Dictionary consisting of skill data, List of education level (string[]), List of U.S. states (string[])
+def get_static_data(request):
+    #Output: JSON dictionary consisting of Dictionary consisting of skill data and List of U.S. states (string[])
     
     #initialize skillValues dictionary
     skillValues = []
-    categories = [Skill.objects.values('category').distinct()]
-    for item in categories:
-        skillValues.append(item)
-        skills = Skill.objects.filter(Skill.category==item)
+    categories = list(Skill.objects.values('category').distinct())
+    for category in categories:
+        catName = category['category']
+        catData = {'category': catName, 'skills': []}
+        skills = list(Skill.objects.filter(category=catName))
         for skill in skills:
-            skillValues.append({'id': skill.pk, 'name':skill.skill_name})
+            catData['skills'].append({'id':skill.pk, 'name': skill.skill_name})
+        skillValues.append(catData)
 
     #initialize locationValues dictionary
     locationValues = []
@@ -56,4 +59,4 @@ def get_static_data():
 
 
     #output:
-    return skillValues, locationValues
+    return JsonResponse({'skill values': skillValues, 'location values': locationValues})
