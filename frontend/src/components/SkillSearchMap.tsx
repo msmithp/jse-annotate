d31f3ae2cd4976d2f72047645a148ee24cd3c674
msmithp <matthewsmith2003@yahoo.com>
2025-03-17 23:20:37 -0400

    Add coloring to SkillSearchMap

--- frontend/src/components/SkillSearchMap.tsx
+++ frontend/src/components/SkillSearchMap.tsx
@@ -1,36 +1,79 @@
 import React from "react";
 import { MapContainer, TileLayer, useMap, GeoJSON } from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
-import L from "leaflet";
 import counties from "../geodata/counties.json";
 
 
-function InnerMap() {
-    const map = useMap();
-
-    
+const countySkills = {
+    state: {
+        stateID: 0,
+        stateName: "Delaware",
+        stateCode: "DE"
+    },
+    countyData: [
+        {countyID: 0, countyName: "Kent", skillID: 5, skillName: "Python"},
+        {countyID: 1, countyName: "New Castle", skillID: 6, skillName: "Go"},
+        {countyID: 2, countyName: "Sussex", skillID: 9, skillName: "Java"}
+    ]
 }
 
-interface SkillSearchMapProps {
+const colors = ["red", "orange", "yellow", "green", "blue", "purple"];
 
+interface SkillSearchMapProps {
+    state: {
+        stateID: number,
+        stateName: string,
+        stateCode: string
+    }
+    countyData: {
+        countyID: number,
+        countyName: string,
+        skillID: number,
+        skillName: string
+    }[]
 }
 
 function SkillSearchMap() {
     // Cast geoData to FeatureCollection
     const geoData = counties as GeoJSON.FeatureCollection;
 
-    const mapOptions = {
+    // Data on all counties in this state
+    const allCounties = countySkills.countyData;
 
-    }
+    let colorDict: { [skillName: string]: string } = {};
+    let colorIdx = 0;
 
     function style(feature: GeoJSON.Feature | undefined) {
-        if (!feature) {
+        if (!feature || !feature.properties) {
             // Return empty dictionary if feature is undefined
             return {};
         }
+
+        let fill = "gray";
+
+        // Check if this county is in the current state
+        if (feature.properties.STATECODE == countySkills.state.stateCode) {
+            // Name of current county (e.g., "Sussex")
+            const countyName = feature.properties.NAME;
+
+            // Get data on the current county
+            const county = allCounties[allCounties.findIndex(item => item.countyName === countyName)];
+
+            // ID of skill mapped to this county
+            const skillID = county.skillID;
+
+            if (!(skillID in colorDict)) {
+                // If this skill is not in the color dictionary, assign it a
+                // new color from the colors list and add it to the dictionary
+                colorDict[skillID] = colors[colorIdx];
+                colorIdx = (colorIdx + 1) % colors.length;
+            }
+
+            fill = colorDict[skillID];
+        }
         
         return {
-            fillColor: "#800026",
+            fillColor: fill,
             weight: 1,
             opacity: 1,
             color: 'white',
