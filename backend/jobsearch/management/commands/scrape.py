82f285f25d36d8429f2a28399e986bfcf5bbf77b
msmithp <matthewsmith2003@yahoo.com>
2025-04-14 19:39:35 -0400

    Create scraper to database pipeline

--- backend/jobsearch/management/commands/scrape.py
+++ backend/jobsearch/management/commands/scrape.py
@@ -1,13 +1,54 @@
-import csv
 from django.core.management.base import BaseCommand
-from jobsearch.models import Job
+from utils.scraper import scrape
 from utils.process_job import process_job
+from jobsearch.models import Job
 
 class Command(BaseCommand):
     help = "Scrapes job data from Indeed and inserts it into the database"
 
     def add_arguments(self, parser):
-        pass
+        parser.add_argument(
+            "num_jobs",
+            type=int,
+            help="Number of jobs to scrape"
+        )
+
+        parser.add_argument(
+            "--nocsv",
+            action="store_true",
+            help="Don't export a CSV of the scraped jobs"
+        )
 
     def handle(self, *args, **options):
-        pass
+        if options["num_jobs"] > 1000:
+            # Anything above 1000 jobs will get you rate-limited
+            raise Exception("Maximum number of jobs (1000) exceeded")
+
+        # Scrape job data
+        job_data = scrape(options["num_jobs"], not options["nocsv"], "./utils/jobs/")
+        
+        # Store jobs and skills in arrays for bulk creation
+        jobs = []
+        jobs_and_skills = []
+
+        # Iterate through rows of scraped job data
+        for _, row in job_data.iterrows():
+            # Convert row from Pandas series to a list
+            row = row.to_list()
+
+            # Convert posting date to a string so it can be parsed by process_job()
+            row[7] = row[7].strftime("%Y-%m-%d")
+
+            # Process job
+            job_and_skill = process_job(row)
+            jobs_and_skills.append(job_and_skill)
+            jobs.append(job_and_skill[0])
+
+        # Create job objects in database
+        Job.objects.bulk_create(jobs)
+
+        # Set skills of jobs
+        for job, skill_list in jobs_and_skills:
+            job.skills.set(skill_list)
+
+        print("Successfully created job data")
--- backend/utils/scraper.py
+++ backend/utils/scraper.py
@@ -7,8 +7,8 @@ verbose = 0      # 0 for only errors, 1 for errors and warnings, 2 for all logs
 append = False   # True to append to a file, False to create or overwrite a file
 num_jobs = 1000  # Number of job listings to scrape
 
-# CSV file for jobs to be saved to
-file_name = f"./jobs/jobs_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.csv"
+# Default file path where CSV will be saved
+default_file_path = "./jobs/"
 
 # List of job titles and keywords to search for
 titles = ["software developer", "software engineer", "data scientist", 
@@ -20,7 +20,9 @@ titles = ["software developer", "software engineer", "data scientist",
 def make_query(titles: list[str]) -> str:
     """
     Turn a list of job titles into a JobSpy Indeed query.
+
     :param titles: A list of jobs as strings
+
     :return: A string with the jobs in quotes and delimited by the string " OR "
     """
     return intercalate(list(map(lambda s: wrap(s, '"'), titles)), " OR ")
@@ -29,8 +31,10 @@ def make_query(titles: list[str]) -> str:
 def wrap(s: str, wrapper: str) -> str:
     """
     Append and prepend a given string to another string.
+
     :param s: String to be wrapped
     :param wrapper: String to be added to the beginning and end of `s`
+
     :return: The wrapped string `wrapper` + `s` + `wrapper`
     """
     return wrapper + s + wrapper
@@ -39,8 +43,10 @@ def wrap(s: str, wrapper: str) -> str:
 def intercalate(lst: list, delim: str) -> str:
     """
     Insert a string between each item in a list and return a string.
+
     :param lst: List of items
     :param delim: String to be inserted in between each item
+
     :return: A string containing the contents of `lst` delimited by `delim`
     """
     n = len(lst)
@@ -57,11 +63,20 @@ def intercalate(lst: list, delim: str) -> str:
     return result
 
 
-def scrape() -> None:
+def scrape(num_jobs: int=10, export: bool=True, file_path: str=None):
     """
-    Scrape jobs from Indeed. Creates or overwrites the file `file_name` if
-    `append` is False. Appends to `file_name` if `append` is True.
+    Scrape jobs from Indeed.
+
+    :param num_jobs: Number of jobs to be scraped. Any number above 1000 is
+                     likely to get you rate-limited.
+    :param export: `True` to export jobs to a CSV file, `False` otherwise
+    :param file_path: File path of exported CSV
+
+    :return: DataFrame containing job data
     """
+    # Name of CSV file to which jobs will be saved
+    file_name = f"jobs_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.csv"
+
     jobs = jobspy.scrape_jobs(
         site_name="indeed",
         search_term=make_query(titles),
@@ -75,12 +90,16 @@ def scrape() -> None:
         print(f"Found {len(jobs)} jobs")
         print(jobs.head())
 
-    if append:
-        jobs.to_csv(file_name, mode='a', quoting=csv.QUOTE_NONNUMERIC,
-                    escapechar="\\", index=False, header=False)
-    else:
-        jobs.to_csv(file_name, quoting=csv.QUOTE_NONNUMERIC, escapechar="\\", index=False)
+    if export and file_path:
+        if append:
+            jobs.to_csv(file_path + file_name, mode='a', quoting=csv.QUOTE_NONNUMERIC,
+                        escapechar="\\", index=False, header=False)
+        else:
+            jobs.to_csv(file_path + file_name, quoting=csv.QUOTE_NONNUMERIC, 
+                        escapechar="\\", index=False)
+
+    return jobs
 
 
 if __name__ == "__main__":
-    scrape()
+    scrape(num_jobs, True, default_file_path)
