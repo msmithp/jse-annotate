b3df02a2cd559b3329f5899cf030a4795f1bd1c5
Matthew Smith <matthewsmith2003@yahoo.com>
2025-05-20 18:20:09 -0400

    Add placeholder HTML injection to Indeed job search page

--- src/background.ts
+++ src/background.ts
@@ -1,19 +1,44 @@
-// let active = false;
+function testInject() {
+    const idPrefix = "mosaic-aboveFullJobDescription";
+    const elem = document.querySelector(`div[id^='${idPrefix}']`);
+    let div = document.createElement("div");
+    div.className = "jobsearch-injectedRequirements";
+    div.innerHTML = "<h2>Requirements</h2><p>Some stuff</p><hr>";
 
-// function makeOrange(color: string): void {
-//     document.body.style.backgroundColor = color;
-// }
+    const alreadyInjected = document.querySelectorAll(
+        "div[class='jobsearch-injectedRequirements']"
+    ).length !== 0;
 
-// chrome.action.onClicked.addListener((tab) => {
-//     active = !active;
-//     const color = active ? 'orange' : 'white';
-//     chrome.scripting.executeScript({
-//         target: {tabId: tab.id ? tab.id : -1},
-//         func: makeOrange,
-//         args: [color]
-//     }).then();
-// });
+    if (!alreadyInjected) {
+        inject(div, elem!);
+    }
+}
 
-let div = document.createElement("div"); 
-document.body.appendChild(div); 
-div.innerText="test123";
+function inject(div: Element, inside: Element) {
+    // Inject `div` inside `inside` element
+    inside.appendChild(div);
+}
+
+// Get right pane of Indeed job search page
+const pane = document.querySelector("div[class^='jobsearch-RightPane']");
+
+const config = {
+    attributes: true,
+    childList: true,
+    subtree: true
+};
+
+function callback(mutationList: MutationRecord[], _: MutationObserver) {
+    for (const mutation of mutationList) {
+        if (mutation.type === "childList") {
+            console.log("A child node has been added or removed");
+            testInject();
+        } else if (mutation.type === "attributes") {
+            console.log(`The ${mutation.attributeName} attribute was modified`);
+        }
+    }
+}
+
+const observer = new MutationObserver(callback);
+
+observer.observe(pane!, config);
